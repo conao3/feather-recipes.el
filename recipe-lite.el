b6116bec683d8982116b1d415846dff6bec36fd2
conao3 <conao3@gmail.com>
2018-12-20 20:08:41 +0900

    add sample recipe file

---
+++ recipe-lite.el
@@ -0,0 +1,23 @@
+(:0blayout
+   (:props
+    (:url "https://github.com/etu/0blayout"
+     :maintainer "Elis \"etu\" Axelsson"
+     :authors ("Elis \"etu\" Axelsson")
+     :keywords ("convenience" "window-management")
+     :commit "873732ddb99a3ec18845a37467ee06bce4e61d87")
+    :type "single"
+    :desc "Layout grouping with ease"
+    :deps nil
+    :ver (20161008 607))
+ :0xc
+  (:props
+   (:url "http://github.com/AdamNiederer/0xc"
+    :commit "12c2c6118c062a49594965c69e6a17bb46339eb2")
+   :deps (:s (1 11 0)
+          :emacs (24 4))
+   :ver (20170126 353))
+ :2048-game
+  (:props
+   (:url "https://bitbucket.org/zck/2048.el")
+   :deps nil
+   :ver (20151026 1933)))

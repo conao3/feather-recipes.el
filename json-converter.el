717e5586560a3c61c4499c510e96fb0f6cc4438c
conao3 <conao3@gmail.com>
2018-12-20 11:54:08 +0900

    add json-converter.el

---
+++ json-converter.el
@@ -0,0 +1,22 @@
+(require 'json)
+
+(let ((json-object-type 'plist)
+      (json-array-type  'list)
+      (json-key-type    'keyword))
+  (let* ((current-dir (file-name-directory
+                       (directory-file-name load-file-name)))
+         (read-file   (concat current-dir (nth 0 command-line-args-left)))
+         (write-file  (concat current-dir (nth 1 command-line-args-left)))
+         (contents))
+    (princ (format "Process completed!!\nRead file: %s\nWrite file: %s\n"
+                   read-file write-file))
+    (if (and (file-writable-p write-file)
+             (file-readable-p read-file))
+        (progn
+          (with-temp-file write-file
+            (setq contents (prin1-to-string (json-read-file read-file)))
+            (insert contents)
+            (insert "\n"))
+          (princ (format "Process completed!!\nRead file: %s\nWrite file: %s\n"
+                         read-file write-file)))
+      (error "File open error"))))

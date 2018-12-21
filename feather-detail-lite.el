e7fd8e152e7ccaa7f6495186f26d2acf6bfd66d1
conao3 <conao3@gmail.com>
2018-12-21 03:50:00 +0000

    Travis CI (job 10.1) [skip ci]

--- feather-detail-lite.el
+++ feather-detail-lite.el
@@ -1,7 +1,7 @@
-#s(hash-table size 6000 test eq rehash-size 1.5 rehash-threshold 0.8125 data 
-   (:zzz-to-char (:ver (20180101 619) :deps (:emacs (24 4) :cl-lib (0 5) :avy (0 3 0)) :url "https://github.com/mrkkrp/zzz-to-char" :description nil :keywords ("convenience") :authors ("Mark Karpov <markkarpov92@gmail.com>") :maintainer "Mark Karpov <markkarpov92@gmail.com>")
-    :zygospore (:ver (20140703 852) :deps nil :url "https://github.com/louiskottmann/zygospore.el" :description nil :keywords nil :authors ("Louis Kottmann <louis.kottmann@gmail.com>") :maintainer "Louis Kottmann <louis.kottmann@gmail.com>")
-    :zweilight-theme (:ver (20170113 605) :deps nil :url "http://github.com/philiparvidsson/zweilight-emacs" :description nil :keywords nil :authors ("Philip Arvidsson <contact@philiparvidsson.com>") :maintainer "Philip Arvidsson <contact@philiparvidsson.com>")
+#s(hash-table size 6000 test eq rehash-size 1.5 rehash-threshold 0.8 data 
+   (:zprint-mode (:ver (20181111 1945) :deps (:emacs (24 3)) :url "https://github.com/pesterhazy/zprint-mode.el" :description nil :keywords ("tools") :authors ("Paulus Esterhazy <pesterhazy@gmail.com>") :maintainer "Paulus Esterhazy <pesterhazy@gmail.com>")
     :ztree (:ver (20180512 1850) :deps (:cl-lib (0)) :url "https://github.com/fourier/ztree" :description nil :keywords ("files" "tools") :authors ("Alexey Veretennikov <alexey.veretennikov@gmail.com>") :maintainer "Alexey Veretennikov <alexey.veretennikov@gmail.com>")
-    :zprint-mode (:ver (20181111 1945) :deps (:emacs (24 3)) :url "https://github.com/pesterhazy/zprint-mode.el" :description nil :keywords ("tools") :authors ("Paulus Esterhazy <pesterhazy@gmail.com>") :maintainer "Paulus Esterhazy <pesterhazy@gmail.com>")
+    :zweilight-theme (:ver (20170113 605) :deps nil :url "http://github.com/philiparvidsson/zweilight-emacs" :description nil :keywords nil :authors ("Philip Arvidsson <contact@philiparvidsson.com>") :maintainer "Philip Arvidsson <contact@philiparvidsson.com>")
+    :zygospore (:ver (20140703 852) :deps nil :url "https://github.com/louiskottmann/zygospore.el" :description nil :keywords nil :authors ("Louis Kottmann <louis.kottmann@gmail.com>") :maintainer "Louis Kottmann <louis.kottmann@gmail.com>")
+    :zzz-to-char (:ver (20180101 619) :deps (:avy (0 3 0) :cl-lib (0 5) :emacs (24 4)) :url "https://github.com/mrkkrp/zzz-to-char" :description nil :keywords ("convenience") :authors ("Mark Karpov <markkarpov92@gmail.com>") :maintainer "Mark Karpov <markkarpov92@gmail.com>")
    ))
--- feather-recipes-lite.el
+++ feather-recipes-lite.el
@@ -1,7 +1,7 @@
-#s(hash-table size 6000 test eq rehash-size 1.5 rehash-threshold 0.8125 data 
-   (:zzz-to-char (:ver (20180101 619) :deps (:emacs (24 4) :cl-lib (0 5) :avy (0 3 0)) :url "https://github.com/mrkkrp/zzz-to-char")
-    :zygospore (:ver (20140703 852) :deps nil :url "https://github.com/louiskottmann/zygospore.el")
-    :zweilight-theme (:ver (20170113 605) :deps nil :url "http://github.com/philiparvidsson/zweilight-emacs")
+#s(hash-table size 6000 test eq rehash-size 1.5 rehash-threshold 0.8 data 
+   (:zprint-mode (:ver (20181111 1945) :deps (:emacs (24 3)) :url "https://github.com/pesterhazy/zprint-mode.el")
     :ztree (:ver (20180512 1850) :deps (:cl-lib (0)) :url "https://github.com/fourier/ztree")
-    :zprint-mode (:ver (20181111 1945) :deps (:emacs (24 3)) :url "https://github.com/pesterhazy/zprint-mode.el")
+    :zweilight-theme (:ver (20170113 605) :deps nil :url "http://github.com/philiparvidsson/zweilight-emacs")
+    :zygospore (:ver (20140703 852) :deps nil :url "https://github.com/louiskottmann/zygospore.el")
+    :zzz-to-char (:ver (20180101 619) :deps (:avy (0 3 0) :cl-lib (0 5) :emacs (24 4)) :url "https://github.com/mrkkrp/zzz-to-char")
    ))

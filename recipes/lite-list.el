363f3fb9e9fb01b020628ebc4946cd2d38a4a7d2
conao3 <conao3@gmail.com>
2018-12-27 21:05:57 +0900

    regenerate recipes

---
+++ recipes/lite-list.el
@@ -0,0 +1,6 @@
+'(zzz-to-char (:ver (20180101 619) :deps (:emacs (24 4) :cl-lib (0 5) :avy (0 3 0)) :fetcher "github" :repo "mrkkrp/zzz-to-char" :files nil)
+  zygospore (:ver (20140703 852) :deps nil :fetcher "github" :repo "LouisKottmann/zygospore.el" :files nil)
+  zweilight-theme (:ver (20170113 605) :deps nil :fetcher "github" :repo "philiparvidsson/Zweilight-Theme-for-Emacs" :files nil)
+  ztree (:ver (20180512 1850) :deps (:cl-lib (0)) :fetcher "github" :repo "fourier/ztree" :files nil)
+  zprint-mode (:ver (20181111 1945) :deps (:emacs (24 3)) :fetcher "github" :repo "pesterhazy/zprint-mode.el" :files nil)
+ )
---
+++ scripts/create-lite-json.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+require 'uri'
+require "json"
+
+# waiting merge pull-request
+# https://github.com/melpa/melpa/pull/5888
+archives = Net::HTTP.get(URI.parse('https://files.conao3.com/feather-recipes/lite-archives.json'))
+recipes = Net::HTTP.get(URI.parse('https://files.conao3.com/feather-recipes/lite-recipes.json'))
+
+hash_archives = JSON.parse(archives)
+hash_recipes = JSON.parse(recipes)
+hash = hash_archives.merge(hash_recipes) {|key, v1, v2| v1.merge(v2)}
+
+puts JSON.dump(hash)

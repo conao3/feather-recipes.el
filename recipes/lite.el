9408edac99b32f412790ae35acd74a5ac72bbb74
conao3 <conao3@gmail.com>
2019-01-05 11:53:26 +0000

    Travis CI (job 47.1) [skip ci]

--- recipes/lite.el
+++ recipes/lite.el
@@ -1,7 +1,7 @@
 #s(hash-table size 65 test eq rehash-size 1.5 rehash-threshold 0.8125 data 
-   (zzz-to-char (:fetcher "github" :repo "mrkkrp/zzz-to-char" :files nil)
-    zygospore (:fetcher "github" :repo "LouisKottmann/zygospore.el" :files nil)
-    zweilight-theme (:fetcher "github" :repo "philiparvidsson/Zweilight-Theme-for-Emacs" :files nil)
-    ztree (:fetcher "github" :repo "fourier/ztree" :files nil)
-    zprint-mode (:fetcher "github" :repo "pesterhazy/zprint-mode.el" :files nil)
+   (zzz-to-char (:fetcher "github" :repo nil :files nil)
+    zygospore (:fetcher "github" :repo nil :files nil)
+    zweilight-theme (:fetcher "github" :repo nil :files nil)
+    ztree (:fetcher "github" :repo nil :files nil)
+    zprint-mode (:fetcher "github" :repo nil :files nil)
    ))

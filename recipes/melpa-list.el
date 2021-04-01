de1060fedab68525fa5349d39422bf420d6dff61
conao3 <conao3@gmail.com>
2021-04-01 06:14:02 +0000

    generate list recipes (job 879.1) [skip ci]

--- recipes/melpa-list.el
+++ recipes/melpa-list.el
@@ -3644,7 +3644,7 @@
   soft-stone-theme (:fetcher "github" :repo "mswift42/soft-stone-theme" :files nil :deps (:emacs (24)))
   solaire-mode (:fetcher "github" :repo "hlissner/emacs-solaire-mode" :files nil :deps (:emacs (24 4) :cl-lib (0 5)))
   solarized-theme (:fetcher "github" :repo "bbatsov/solarized-emacs" :files nil :deps (:emacs (24 1) :dash (2 16)))
-  solidity-flycheck (:fetcher "github" :repo "ethereum/emacs-solidity" :files ("solidity-flycheck.el") :deps (:flycheck (32 -4) :solidity-mode (0 1 9)))
+  solidity-flycheck (:fetcher "github" :repo "ethereum/emacs-solidity" :files ("solidity-flycheck.el") :deps (:flycheck (32 -4) :solidity-mode (0 1 9) :dash (2 17 0)))
   solidity-mode (:fetcher "github" :repo "ethereum/emacs-solidity" :files ("//:defaults//" (:exclude ("company-solidity.el" "solidity-flycheck.el"))) :deps nil)
   sonic-pi (:fetcher "github" :repo "repl-electric/sonic-pi.el" :files nil :deps (:cl-lib (0 5) :osc (0 1) :dash (2 2 0) :emacs (24) :highlight (0)))
   soothe-theme (:fetcher "github" :repo "emacsfodder/emacs-soothe-theme" :files nil :deps (:emacs (24 1)))
